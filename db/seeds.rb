55298ee6cb939111f78a3bf708d6797fb77678e2
codebit-00 <code.ciencia@gmail.com>
2021-04-21 16:03:33 -0500

    Add a seed to create 47 devices with a unique name

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Device.destroy_all
+User.destroy_all
+ReparationRequest.destroy_all
+Store.destroy_all
+
+# Create 47 devices sorted in alphabetical order
+puts "Start seeding devices"
+  devices = ["Air Conditioner", "Air Purifier", "Blender", "Bread Machine", "Built-in Dishwasher", "Ceiling Fan", "Coffee Grinder", "Coffee Maker", 
+             "Deep Fryer", "Dehumidifier", "Domestic Robot", "Dryer", "Food Dehydrator", "Food Processor", "Freezer", "Garbage Disposal", "Generator", 
+             "Home theater", "Hood Smoke Extractor", "Humidifier", "Ice Cream Maker", "Instant Pot", "Iron", "Juicer", "Microwave", "Mobile phone",
+             "Oven", "Panini Maker", "Popcorn Machine", "Portable Dishwasher", "Refrigerator", "Rice Cooker", "Sewing Machine", "Slow Cooker", "Smart TV", 
+             "Smoker", "Space Heater", "Stand Mixer", "Steam Cleaner", "Toaster", "Toaster Oven", "Vacuum", "Waffle Iron", "Washing Machine", 
+             "Water Heater", "Water Purifier", "Yogurt Maker"]
+  devices.each do |device|
+    Device.find_or_create_by({name: device})
+  end 
+puts "End seeding devices"
