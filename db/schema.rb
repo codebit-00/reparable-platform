75aec4e569831b3d7db6585342b2d91801d5a06c
codebit-00 <code.ciencia@gmail.com>
2021-04-21 15:42:04 -0500

    Add a unique index to priority column in the stores table and update the schema file with it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_203248) do
+ActiveRecord::Schema.define(version: 2021_04_21_204009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_203248) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["priority"], name: "index_stores_on_priority", unique: true
   end
 
   create_table "users", force: :cascade do |t|
