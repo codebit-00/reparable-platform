c1ad1040cf0b00c516505dd317a53608eacee688
codebit-00 <code.ciencia@gmail.com>
2021-04-26 17:04:06 -0500

    Add a route for the reparation_requests controller to be able update the status of a specific reparation request using a link as button

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   resources :reparation_requests, except: %i[edit destroy]
 
+  resources :reparation_requests do
+    member do
+      patch :change_status
+    end
+  end
+
   get '/reparation_requests' => 'reparation_requests#index', :as => :user_root
 
   root 'reparation_requests#new'
